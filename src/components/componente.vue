efd2e5abd42717a14498aab7319d8386aeb1ad61
Augusto <42296932+4august@users.noreply.github.com>
2023-12-06 14:44:56 -0400

    instalando pinia, vue-router e element-plus

---
+++ src/components/componente.vue
@@ -0,0 +1,7 @@
+<template>
+<el-row justify="center">
+    <el-col :span="24">
+      <h1>Flapp</h1>
+    </el-col>
+  </el-row>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<script setup>
+    import compoente from '../components/componente.vue'
+</script>
+<template>
+    <compoente/>
+</template>
